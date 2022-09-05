123c62b704d567a19e4f3ade7b79da457d78a255
Ademilson Silva <ademilson.w6@hotmail.com>
2022-09-05 19:54:19 -0300

    Project Created

---
+++ backend/src/test/java/Projeto/da/semana/Spring/Boot/React/da/Dev/Superior/DsmetaApplicationTests.java
@@ -0,0 +1,13 @@
+package Projeto.da.semana.Spring.Boot.React.da.Dev.Superior;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
